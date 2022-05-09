284f5e64d51e9e6d65ec0257de72de8d92c52e22
Tobias Michalski <tobias.michalski@bsk-consulting.de>
2022-05-09 17:04:18 +0200

    fix: Cleaning up Tabs

--- src/commandProvider.ts
+++ src/commandProvider.ts
@@ -25,13 +25,21 @@ export async function addTagQuickpick() {
         const tagsRegex = new RegExp("^tags:$\n(\\s*-.+$)*", "m")
         let docText = vscode.window.activeTextEditor?.document.getText()!
         let tags = tagsRegex.exec(docText)
+        let tab = "    "
+        if (
+            vscode.window.activeTextEditor?.options.tabSize &&
+            typeof vscode.window.activeTextEditor?.options.tabSize !== "string"
+        ) {
+            tab = ` `.repeat(vscode.window.activeTextEditor?.options.tabSize)
+        }
         if (tags) {
             let index = docText.indexOf(tags[0]) + tags[0].length
+
             let pos = vscode.window.activeTextEditor?.document.positionAt(index)
             vscode.window.activeTextEditor?.edit(textEdit => {
                 textEdit.insert(
                     vscode.window.activeTextEditor?.document.positionAt(index)!,
-                    `\n    - attack.${target?.label.match("(.+?) -")![1].toLowerCase()}`,
+                    `\n${tab}- attack.${target?.label.match("(.+?) -")![1].toLowerCase()}`,
                 )
             })
         } else {
@@ -41,7 +49,7 @@ export async function addTagQuickpick() {
             vscode.window.activeTextEditor?.edit(textEdit => {
                 textEdit.insert(
                     vscode.window.activeTextEditor?.selection.end!,
-                    `    - attack.${target?.label.match("(.+?) -")![1].toLowerCase()}`,
+                    `${tab}- attack.${target?.label.match("(.+?) -")![1].toLowerCase()}`,
                 )
             })
         }
