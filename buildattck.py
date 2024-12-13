9c2f1257335deb8b1dc05fe97e8109f8b89d26cf
Mahmoud Soheem <84213915+msoheem@users.noreply.github.com>
2024-12-13 09:22:49 +0200

    Update buildattck.py
    Adding the updated version of Mitre enterprise attack techniques

--- buildattck.py
+++ buildattck.py
@@ -4,16 +4,24 @@ import csv
 finobj = []
 
 # Get this at https://github.com/mitre/cti/blob/master/enterprise-attack/enterprise-attack.json
-with open("enterprise-attack.json") as f:
-    data = json.load(f)
-    for i in data["objects"]:
+#with open("enterprise-attack.json") as f:
+#    data = json.load(f)
+#    for i in data["objects"]:
         # if i["type"] == "attack-pattern":
-        if "external_references" in i:
-            for id in i["external_references"]:
-                if "external_id" in id and "description" in i:
-                    finobj.append(
-                        {"name": i["name"], "description": i["description"], "tag": id["external_id"], "url": id["url"]})
+#        if "external_references" in i:
+#            for id in i["external_references"]:
+#                if "external_id" in id and "description" in i:
+#                    finobj.append(
+#                        {"name": i["name"], "description": i["description"], "tag": id["external_id"], "url": id["url"]})
 
+with open("enterprise-attack-v16.1-techniques.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[0]
+        name = row[2]
+        desc = row[3]
+        url = row[4]
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
 
 # Get this at https://d3fend.mitre.org/ontologies/d3fend.csv
 with open("d3fend.csv") as file:
