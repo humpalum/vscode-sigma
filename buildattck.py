62f60d6537b2bbe5e10703a480d6d5951e7355a2
Mahmoud Soheem <84213915+msoheem@users.noreply.github.com>
2024-12-22 11:14:13 +0200

    Update buildattck.py
    adding tactics, software, datasource, campaign, mitigations, groups

--- buildattck.py
+++ buildattck.py
@@ -14,6 +14,21 @@ finobj = []
 #                    finobj.append(
 #                        {"name": i["name"], "description": i["description"], "tag": id["external_id"], "url": id["url"]})
 
+################## collecting tactics ##################
+# Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-tactics.xlsx
+# Then save as csv the first sheet
+with open("enterprise-attack-v16.1-tactics.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[0]
+        name = row[2]
+        desc = row[3]
+        url = row[4]
+        if id == "ID":   # skipping header
+            continue
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
+
+################## collecting techniques ##################
 # Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-techniques.xlsx
 # Then save as csv the first sheet
 with open("enterprise-attack-v16.1-techniques.csv", mode="r") as f:
@@ -23,6 +38,79 @@ with open("enterprise-attack-v16.1-techniques.csv", mode="r") as f:
         name = row[2]
         desc = row[3]
         url = row[4]
+        if id == "ID": # skipping header
+            continue
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
+
+################## collecting software ##################
+# Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-software.xlsx
+# Then save as csv the first sheet
+with open("enterprise-attack-v16.1-software.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[0]
+        name = row[2]
+        desc = row[3]
+        url = row[4]
+        if id == "ID": # skipping header
+            continue
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
+
+################## collecting groups ##################
+# Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-groups.xlsx
+# Then save as csv the first sheet
+with open("enterprise-attack-v16.1-groups.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[0]
+        name = row[2]
+        desc = row[3]
+        url = row[4]
+        if id == "ID": # skipping header
+            continue
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
+            
+################## collecting campaigns ##################
+# Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-campaigns.xlsx
+# Then save as csv the first sheet
+with open("enterprise-attack-v16.1-campaigns.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[0]
+        name = row[2]
+        desc = row[3]
+        url = row[4]
+        if id == "ID": # skipping header
+            continue
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
+            
+
+################## collecting datasources ##################
+# Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-datasources.xlsx
+# Then save as csv the first sheet
+with open("enterprise-attack-v16.1-datasources.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[1]
+        name = row[0]
+        desc = row[3]
+        url = row[10]
+        if id == "" or id == "ID": # skipping header and empty datasource
+            continue
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
+            
+################## collecting mitigations ##################
+# Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-mitigations.xlsx
+# Then save as csv the first sheet
+with open("enterprise-attack-v16.1-mitigations.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[0]
+        name = row[2]
+        desc = row[3]
+        url = row[4]
+        if id == "ID": # skipping header
+            continue
         finobj.append({"name": name, "description": desc, "tag":id, "url":url})
 
 # Get this at https://d3fend.mitre.org/ontologies/d3fend.csv
@@ -36,5 +124,5 @@ with open("d3fend.csv") as file:
 
 
 finobj.sort(key=lambda x: x["tag"])
-with open("./src/techniques.json", 'w') as nf:
+with open("techniques.json", 'w') as nf:
     nf.write(json.dumps(finobj, indent=4))
