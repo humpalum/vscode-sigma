03159b4c0435d629de762fa7895fc1c3158a4cca
Tobias Michalski <humpalum@users.noreply.github.com>
2025-03-26 09:47:40 +0100

    Mr31 (#32)
    * Update commandProvider.ts
    
    Updated RegExp for finding "tags:" in the active editor as it works well for Linux system however, it does not work as expected in Windows due to the differences in line ending.
    
    * Updated version of Mitre enterprise techniques
    
    * Update buildattck.py
    
    Adding the updated version of Mitre enterprise attack techniques
    
    * Update buildattck.py
    
    adding reference
    
    * Update buildattck.py
    
    adding tactics, software, datasource, campaign, mitigations, groups
    
    * Add files via upload
    
    adding mitre files.
    
    * Update buildattck.py
    
    * fix: Encoding
    
    * chore: Bump verison
    
    ---------
    
    Co-authored-by: Mahmoud Soheem <84213915+msoheem@users.noreply.github.com>
    Co-authored-by: Tobias Michalski <tobias.michalski@bsk-consulting.de>

--- buildattck.py
+++ buildattck.py
@@ -4,16 +4,114 @@ import csv
 finobj = []
 
 # Get this at https://github.com/mitre/cti/blob/master/enterprise-attack/enterprise-attack.json
-with open("enterprise-attack.json") as f:
-    data = json.load(f)
-    for i in data["objects"]:
+#with open("enterprise-attack.json") as f:
+#    data = json.load(f)
+#    for i in data["objects"]:
         # if i["type"] == "attack-pattern":
-        if "external_references" in i:
-            for id in i["external_references"]:
-                if "external_id" in id and "description" in i:
-                    finobj.append(
-                        {"name": i["name"], "description": i["description"], "tag": id["external_id"], "url": id["url"]})
+#        if "external_references" in i:
+#            for id in i["external_references"]:
+#                if "external_id" in id and "description" in i:
+#                    finobj.append(
+#                        {"name": i["name"], "description": i["description"], "tag": id["external_id"], "url": id["url"]})
 
+################## collecting tactics ##################
+# Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-tactics.xlsx
+# Then save as csv the first sheet
+with open("enterprise-attack-v16.1-tactics.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[0]
+        name = row[2]
+        desc = row[3]
+        url = row[4]
+        if id == "ID":   # skipping header
+            continue
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
+
+################## collecting techniques ##################
+# Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-techniques.xlsx
+# Then save as csv the first sheet
+with open("enterprise-attack-v16.1-techniques.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[0]
+        name = row[2]
+        desc = row[3]
+        url = row[4]
+        if id == "ID": # skipping header
+            continue
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
+
+################## collecting software ##################
+# Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-software.xlsx
+# Then save as csv the first sheet
+with open("enterprise-attack-v16.1-software.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[0]
+        name = row[2]
+        desc = row[3]
+        url = row[4]
+        if id == "ID": # skipping header
+            continue
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
+
+################## collecting groups ##################
+# Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-groups.xlsx
+# Then save as csv the first sheet
+with open("enterprise-attack-v16.1-groups.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[0]
+        name = row[2]
+        desc = row[3]
+        url = row[4]
+        if id == "ID": # skipping header
+            continue
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
+            
+################## collecting campaigns ##################
+# Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-campaigns.xlsx
+# Then save as csv the first sheet
+with open("enterprise-attack-v16.1-campaigns.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[0]
+        name = row[2]
+        desc = row[3]
+        url = row[4]
+        if id == "ID": # skipping header
+            continue
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
+            
+
+################## collecting datasources ##################
+# Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-datasources.xlsx
+# Then save as csv the first sheet
+with open("enterprise-attack-v16.1-datasources.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[1]
+        name = row[0]
+        desc = row[3]
+        url = row[10]
+        if id == "" or id == "ID": # skipping header and empty datasource
+            continue
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
+            
+################## collecting mitigations ##################
+# Get this at https://attack.mitre.org/docs/enterprise-attack-v16.1/enterprise-attack-v16.1-mitigations.xlsx
+# Then save as csv the first sheet
+with open("enterprise-attack-v16.1-mitigations.csv", mode="r") as f:
+    data = csv.reader(f, delimiter=',')
+    for row in data:
+        id = row[0]
+        name = row[2]
+        desc = row[3]
+        url = row[4]
+        if id == "ID": # skipping header
+            continue
+        finobj.append({"name": name, "description": desc, "tag":id, "url":url})
 
 # Get this at https://d3fend.mitre.org/ontologies/d3fend.csv
 with open("d3fend.csv") as file:
